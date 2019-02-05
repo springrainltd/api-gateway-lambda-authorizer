65d9c805acc8b948d8dc737572c26a432eed92c5
ratul <asefr@pmamediagroup.com>
2019-02-05 08:47:36 +0600

    updated

--- index.js
+++ index.js
@@ -35,50 +35,53 @@ var generateDeny = function(principalId, resource) {
 const jwt = require("jsonwebtoken");
 const variables = require("./variables");
 exports.handler = function(event, context, callback) {
-  console.log("Received event:", JSON.stringify(event, null, 2));
+  try {
+    console.log("Received event:", JSON.stringify(event, null, 2));
 
-  // A simple REQUEST authorizer example to demonstrate how to use request
-  // parameters to allow or deny a request. In this example, a request is
-  // authorized if the client-supplied HeaderAuth1 header, QueryString1 query parameter,
-  // stage variable of StageVar1 and the accountId in the request context all match
-  // specified values of 'headerValue1', 'queryValue1', 'stageValue1', and
-  // '123456789012', respectively.
+    // A simple REQUEST authorizer example to demonstrate how to use request
+    // parameters to allow or deny a request. In this example, a request is
+    // authorized if the client-supplied HeaderAuth1 header, QueryString1 query parameter,
+    // stage variable of StageVar1 and the accountId in the request context all match
+    // specified values of 'headerValue1', 'queryValue1', 'stageValue1', and
+    // '123456789012', respectively.
 
-  // Retrieve request parameters from the Lambda function input:
-  var headers = event.headers;
-  console.log("headers: \n", headers);
+    // Retrieve request parameters from the Lambda function input:
+    var headers = event.headers;
+    console.log("headers: \n", headers);
 
-  const token = headers.Authorization;
-  const tokenVerified = jwt.verify(token, variables.jwtSecret);
-  console.log("verified token: ", tokenVerified);
+    const token = headers.Authorization;
+    const tokenVerified = jwt.verify(token, variables.jwtSecret);
+    console.log("verified token: ", tokenVerified);
 
-  // var queryStringParameters = event.queryStringParameters;
-  // var pathParameters = event.pathParameters;
-  // var stageVariables = event.stageVariables;
-  // var requestContext = event.requestContext;
+    // var queryStringParameters = event.queryStringParameters;
+    // var pathParameters = event.pathParameters;
+    // var stageVariables = event.stageVariables;
+    // var requestContext = event.requestContext;
 
-  // Parse the input for the parameter values
-  var tmp = event.methodArn.split(":");
-  var apiGatewayArnTmp = tmp[5].split("/");
-  // var awsAccountId = tmp[4];
-  // var region = tmp[3];
-  // var restApiId = apiGatewayArnTmp[0];
-  var stage = apiGatewayArnTmp[1];
-  // var method = apiGatewayArnTmp[2];
-  var resource = "/"; // root resource
-  if (apiGatewayArnTmp[3]) {
-    resource += apiGatewayArnTmp[3];
-  }
-  console.log("stage: ", stage);
-  console.log("resource: ", resource);
+    // Parse the input for the parameter values
+    var tmp = event.methodArn.split(":");
+    var apiGatewayArnTmp = tmp[5].split("/");
+    // var awsAccountId = tmp[4];
+    // var region = tmp[3];
+    // var restApiId = apiGatewayArnTmp[0];
+    var stage = apiGatewayArnTmp[1];
+    // var method = apiGatewayArnTmp[2];
+    var resource = "/"; // root resource
+    if (apiGatewayArnTmp[3]) {
+      resource += apiGatewayArnTmp[3];
+    }
+    console.log("stage: ", stage);
+    console.log("resource: ", resource);
 
-  // Perform authorization to return the Allow policy for correct parameters and
-  // the 'Unauthorized' error, otherwise.
-  var authResponse = {};
-  var condition = {};
-  condition.IpAddress = {};
+    // Perform authorization to return the Allow policy for correct parameters and
+    // the 'Unauthorized' error, otherwise.
+    var authResponse = {};
+    var condition = {};
+    condition.IpAddress = {};
 
-  if (headers.Authorization !== "") {
+    if (headers.Authorization !== "") {
+      throw { status: 400, message: "Authorization header not found" };
+    }
     callback(
       null,
       generateAllow(
@@ -87,7 +90,8 @@ exports.handler = function(event, context, callback) {
         tokenVerified
       )
     );
-  } else {
+  } catch (error) {
+    console.log(`error \n ${error}`);
     callback("Unauthorized");
   }
 };
