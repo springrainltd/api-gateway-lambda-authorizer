91816606aeea6b9fab29b6d2d0eefe1d27977cbb
ratul <asefr@pmamediagroup.com>
2019-02-05 23:49:35 +0600

    updated

--- variables/index.js
+++ variables/index.js
@@ -1,9 +1,7 @@
 const apiServiceUrl = process.env.API_SERVICE_URL;
-const apiToken = process.env.API_TOKEN;
 const jwtSecret = process.env.JWT_SECRET_KEY;
 
 module.exports = {
   apiServiceUrl,
-  apiToken,
   jwtSecret
 };
